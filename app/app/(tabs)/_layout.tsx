9e57065c887d9afafbd5647faa6a34ed2bf9b81e
Lou <wangxuelu1997@outlook.com>
2025-02-10 22:15:41 +0800

    feat: app login

--- app/app/(tabs)/_layout.tsx
+++ app/app/(tabs)/_layout.tsx
@@ -1,56 +1,62 @@
-import { Tabs } from "expo-router";
+import { useEffect } from "react";
+import { Tabs, useRouter } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 import AntDesign from "@expo/vector-icons/AntDesign";
-import useGlobalStore from "@/store";
 import { View } from "react-native";
+
+import useGlobalStore from "@/store";
 
 export default function TabLayout() {
   const { systemColorScheme } = useGlobalStore();
-  return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-        tabBarActiveTintColor: "#FB923C",
-        tabBarStyle: {
-          shadowColor: "transparent",
-          borderTopWidth: 1,
-          borderTopColor: "#f0f0f0",
-        },
-      }}
-    >
-      <Tabs.Screen
-        name="recipe"
-        options={{
-          title: "食谱",
-          tabBarIcon: ({ color }) => (
-            <AntDesign name="book" size={24} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "识别",
-          tabBarIcon: ({ color }) => (
-            <AntDesign name="camerao" size={24} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="setting"
-        options={{
-          title: "我的",
-          tabBarIcon: ({ color }) => (
-            <AntDesign name="user" size={24} color={color} />
-          ),
-        }}
-      />
+  const router = useRouter();
 
-      <Tabs.Screen
-        name="login"
-        options={{
-          href: null,
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/login");
+    }, 1000);
+  }, []);
+
+  return (
+    <SafeAreaView edges={["top"]} style={{ flex: 1 }}>
+      <Tabs
+        screenOptions={{
+          headerShown: false,
+          tabBarActiveTintColor: "#FB923C",
+          tabBarStyle: {
+            shadowColor: "transparent",
+            borderTopWidth: 1,
+            borderTopColor: "#f0f0f0",
+          },
         }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="recipe"
+          options={{
+            title: "食谱",
+            tabBarIcon: ({ color }) => (
+              <AntDesign name="book" size={24} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "识别",
+            tabBarIcon: ({ color }) => (
+              <AntDesign name="camerao" size={24} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="setting"
+          options={{
+            title: "我的",
+            tabBarIcon: ({ color }) => (
+              <AntDesign name="user" size={24} color={color} />
+            ),
+          }}
+        />
+      </Tabs>
+    </SafeAreaView>
   );
 }
--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { Stack } from "expo-router";
-import { SafeAreaView } from "react-native-safe-area-context";
 import { ApplicationProvider } from "@ui-kitten/components";
 import * as eva from "@eva-design/eva";
 import { useColorScheme, Appearance } from "react-native";
@@ -36,16 +35,15 @@ export default function RootLayout() {
         ...theme,
       }}
     >
-      <SafeAreaView edges={["top"]} style={{ flex: 1 }}>
-        <Stack
-          screenOptions={{
-            headerShown: false,
-          }}
-        >
-          <Stack.Screen name="(tabs)" />
-          <Stack.Screen name="+not-found" />
-        </Stack>
-      </SafeAreaView>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="login" />
+        <Stack.Screen name="+not-found" />
+      </Stack>
     </ApplicationProvider>
   );
 }
